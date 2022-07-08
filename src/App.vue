f6e6eae2dd963cb1287e02de18653abc276fd48f
Frowingg <edbico0@gmail.com>
2022-07-08 15:53:59 +0200

    base layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <header>
+      <img src="./assets/logo.png" alt="">
+    </header>
+
+    <main>
+
+    </main>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+header{
+  background-color: #2e3a46;
+  padding: 10px 15px;
+  height: 60px;
+}
+header img{
+  width: 40px;
+  height: 40px;
+}
+main{
+  background-color: #1e2d3b;
+  height: calc(100vh - 60px);
 }
 </style>
